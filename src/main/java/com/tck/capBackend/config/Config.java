31dfb1171a2f8ece44dcc2665ae01de788d7e511
thinktinker <martinleongmo@gmail.com>
2025-06-16 07:15:40 +0800

    fix image display process

--- src/main/java/com/tck/capBackend/config/Config.java
+++ src/main/java/com/tck/capBackend/config/Config.java
@@ -1,5 +1,6 @@
 package com.tck.capBackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Config implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         // Map the URL Path to the external registry
-        registry
-                .addResourceHandler("/uploads/images")
-                .addResourceLocations("file:/uploads/images");
-
+        registry.addResourceHandler("/" + uploadDir + "/**")
+                .addResourceLocations("file:" + uploadDir + "/");
     }
 }
